ba612455681c4a512c39747db3d0541c02691ed6
24javeriaejaz <107063236+24javeriaejaz@users.noreply.github.com>
2022-09-09 19:31:11 +0500

    Update main.py

--- Models/DeepLabV3Plus/main.py
+++ Models/DeepLabV3Plus/main.py
@@ -124,7 +124,7 @@ def get_dataset(opts):
         train_dst = VOCSegmentation(root=opts.data_root, year=opts.year,
                                     image_set='train', download=opts.download, transform=train_transform)
         val_dst = VOCSegmentation(root=opts.data_root, year=opts.year,
-                                  image_set='val', download=False, transform=val_transform)
+                                  image_set='val', download=True, transform=val_transform)
 
     if opts.dataset == 'cityscapes':
         train_transform = et.ExtCompose([
